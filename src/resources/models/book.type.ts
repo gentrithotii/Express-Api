fb33e8c283ff67f4609663611219d6c903e7e638
Gentrithotii <Gentrithoti@hotmail.com>
2022-08-26 09:54:02 +0200

    Added samples into array

--- src/resources/models/book.type.ts
+++ src/resources/models/book.type.ts
@@ -1,19 +1,29 @@
 
 
 
-export interface Book{
+ export interface Book{
     bookId: string;
     bookName: string;
     bookAuthor: String;
     bookPages: string;
 }
 
+
+
 //Data for test without db
 
-export const books: Book[] = [];
+
+export const books: Book[] = [
+{bookId: "1", bookName: "Genida", bookAuthor: "Lala", bookPages:"340"},
+{bookId: "2", bookName: "Lord of the Rings", bookAuthor: "Genci", bookPages:"520"},
+{bookId: "3", bookName: "Genida", bookAuthor: "Emilia", bookPages:"640"},
+{bookId: "4", bookName: "Genida", bookAuthor: "Gonxhe", bookPages:"1240"},
+];
+
+
 
 
 
 export function getListOfBooks(){
-
+    
 }
