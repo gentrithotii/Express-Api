fec50650fbcc733c05854125ba381034c6a8970d
Gentrithotii <Gentrithoti@hotmail.com>
2022-08-26 22:28:43 +0200

    Name change

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import itemsRouter from './resources/items/Books.router';
+import booksRouter from './resources/items/Books.router';
 
 const app = express();
 app.use(express.json());
-app.use("/items", itemsRouter);
+app.use("/books", booksRouter);
 
 
 
